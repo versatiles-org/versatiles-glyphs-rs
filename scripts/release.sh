e5fd3759551d8f237b6acefd84aa16411d58772d
Michael Kreil <github@michael-kreil.de>
2025-02-27 16:26:08 +0100

    build: add release script

---
+++ scripts/release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")/.."
+
+# cargo install toml-cli
+
+set -e
+
+RED="\033[1;31m"
+GRE="\033[1;32m"
+END="\033[0m"
+
+if [ -z "$1" ]; then
+	echo "❗️ Need argument for bumping version: \"patch\", \"minor\" or \"major\""
+	exit 1
+fi
+
+# check cargo
+./scripts/check.sh
+if [ $? -ne 0 ]; then
+	echo "❗️ Check failed!"
+	exit 1
+fi
+
+# publish to crates.io
+cargo release "$1" --execute --no-verify --sign-commit --workspace
