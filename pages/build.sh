668de584ff96b33795fd275a8cf9f947c44814bb
Michael Kreil <github@michael-kreil.de>
2025-02-28 19:38:41 +0100

    ci: add font web demo

---
+++ pages/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+cd "$(dirname "$0")/.."
+
+cargo build -q --bins --features=cli --release
+target/release/versatiles_glyphs convert -o pages/web/assets/glyphs/noto_sans_regular testdata/Noto\ Sans/*.ttf
