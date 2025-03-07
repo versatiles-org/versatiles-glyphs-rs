0249cf2601d36cc6b0342470caf07452af441a39
Michael Kreil <github@michael-kreil.de>
2025-03-07 09:06:02 +0100

    build: improve check script

--- scripts/check.sh
+++ scripts/check.sh
@@ -4,7 +4,30 @@ cd "$(dirname "$0")/.."
 
 PROJECT_DIR=$(pwd)
 
-cargo fmt -- --check
-cargo check --all-features --all-targets
-cargo clippy --all-features --all-targets
-cargo test --all-features --all-targets
+echo "cargo fmt"
+result=$(cargo fmt -- --check 2>&1)
+if [ $? -ne 0 ]; then
+	echo -e "$result\nERROR DURING: cargo fmt"
+	exit 1
+fi
+
+echo "cargo check"
+result=$(cargo check --workspace --all-features --all-targets 2>&1)
+if [ $? -ne 0 ]; then
+	echo -e "$result\nERROR DURING: cargo check"
+	exit 1
+fi
+
+echo "cargo clippy"
+result=$(cargo clippy --all-features --all-targets -- -D warnings 2>&1)
+if [ $? -ne 0 ]; then
+	echo -e "$result\nERROR DURING: cargo clippy $1"
+	exit 1
+fi
+
+echo "cargo test"
+result=$(cargo test --all-features --all-targets 2>&1)
+if [ $? -ne 0 ]; then
+	echo -e "$result\nERROR DURING: cargo test $1"
+	exit 1
+fi
