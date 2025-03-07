409ccbc480fb169790cfcfbf319efb2273fc33ea
Michael Kreil <github@michael-kreil.de>
2025-03-07 08:27:45 +0100

    refactor: remove unused code

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,10 +2,8 @@
 mod decode_bitmap;
 mod output_directory;
 mod progress_bar;
-mod regex;
 
 #[cfg(test)]
 pub use decode_bitmap::*;
 pub use output_directory::*;
 pub use progress_bar::*;
-pub use regex::*;
--- src/utils/regex.rs
+++
@@ -1,6 +0,0 @@
-use regex_lite::Regex;
-
-pub fn replace_all(text: &str, regex: &str, replacement: &str) -> String {
-	let re = Regex::new(regex).unwrap();
-	re.replace_all(text, replacement).to_string()
-}
