98eed26e16488389684c73f2708af306efe806d0
Michael Kreil <github@michael-kreil.de>
2025-03-08 14:05:42 +0100

    refactor: rename to result.rs

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -7,18 +7,18 @@ const BUFFER: i32 = 3;
 const CUTOFF: f64 = 0.25 * 256.0;
 
 mod render_glyph;
-mod render_result;
 #[cfg(test)]
 mod renderer_dummy;
 mod renderer_precise;
+mod result;
 mod ring_builder;
 mod rtree_segments;
 mod traits;
 
 pub use render_glyph::render_glyph;
-pub use render_result::RenderResult;
 #[cfg(test)]
 pub use renderer_dummy::RendererDummy;
 pub use renderer_precise::RendererPrecise;
+pub use result::RenderResult;
 pub use ring_builder::RingBuilder;
 pub use traits::RendererTrait;
