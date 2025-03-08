1db1bcf44e1fdef1c00d9b19ad196f49fe1619fa
Michael Kreil <github@michael-kreil.de>
2025-03-08 15:45:35 +0100

    build: fix selected features

--- pages/build.sh
+++ pages/build.sh
@@ -2,5 +2,5 @@
 set -e
 cd "$(dirname "$0")/.."
 
-cargo build -q --bins --features=cli --release
+cargo build -q --bins --release
 target/release/versatiles_glyphs merge -o pages/web/assets/glyphs/ testdata/Noto\ Sans/*.ttf
--- scripts/test_recurse.sh
+++ scripts/test_recurse.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-cargo build -q --bins --features=cli
+cargo build -q --bins
 # ./target/debug/versatiles_glyphs recurse -t ../versatiles-fonts/fonts/ | gzip -9 > fonts.tar.gz
 ./target/debug/versatiles_glyphs recurse -t ../versatiles-fonts/fonts/ | gzip -9 > fonts.tar.gz
 
-# cargo build -q --bins --features=cli --release
+# cargo build -q --bins --release
 # time ./target/release/versatiles_glyphs convert -o temp ../versatiles-fonts/fonts/Noto\ Sans/Noto\ Sans\ *Regular.ttf
