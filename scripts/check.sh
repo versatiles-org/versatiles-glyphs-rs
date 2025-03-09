19c6a96e5a4705737943d5d4433bd92d4a73d345
Michael Kreil <github@michael-kreil.de>
2025-03-09 18:50:42 +0100

    build: add color

--- scripts/check.sh
+++ scripts/check.sh
@@ -2,28 +2,28 @@
 cd "$(dirname "$0")/.."
 
 echo "cargo fmt"
-result=$(cargo fmt -- --check 2>&1)
+result=$(CARGO_TERM_COLOR=always cargo fmt -- --check 2>&1)
 if [ "$result" != "" ]; then
 	echo -e "$result\nERROR DURING: cargo fmt"
 	exit 1
 fi
 
 echo "cargo check"
-result=$(cargo check --workspace --all-features --all-targets 2>&1)
+result=$(CARGO_TERM_COLOR=always cargo check --workspace --all-features --all-targets 2>&1)
 if [ $? -ne 0 ]; then
 	echo -e "$result\nERROR DURING: cargo check"
 	exit 1
 fi
 
 echo "cargo clippy"
-result=$(cargo clippy --all-features --all-targets -- -D warnings 2>&1)
+result=$(CARGO_TERM_COLOR=always cargo clippy --all-features --all-targets -- -W missing_docs -D warnings 2>&1)
 if [ $? -ne 0 ]; then
 	echo -e "$result\nERROR DURING: cargo clippy $1"
 	exit 1
 fi
 
 echo "cargo test"
-result=$(cargo test --all-features --all-targets 2>&1)
+result=$(CARGO_TERM_COLOR=always cargo test --all-features --all-targets 2>&1)
 if [ $? -ne 0 ]; then
 	echo -e "$result\nERROR DURING: cargo test $1"
 	exit 1
